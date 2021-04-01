1b3286009b69e436ce8bd653b9b78f6b890967eb
kociarownica <anastasiyaholupova@gmail.com>
2021-04-01 14:54:20 +0300

    финальное слияние

--- AbstractScene.cpp
+++ AbstractScene.cpp
@@ -1,4 +1,4 @@
-#include <AbstractScene.h>
+#include "AbstractScene.h"
 
 AbstractScene::AbstractScene(){
     std::cout << "Конструктор абстрактной сцены." << "\n";
@@ -18,6 +18,7 @@ AbstractScene::~AbstractScene(){
 int AbstractScene::addObject(Object* object){
     map_objects[count_id] = object;
     count_id++;
+    return 0;
 }   
 
 int AbstractScene::addObject(int x, int y, String name){
@@ -42,7 +43,7 @@ void AbstractScene::clearDead(){
         if(i.second->health <= 0){
             to_remove.push_back(i.first);                                           // Добавляем в очередь на удаление
 
-            if(i.second->name == "Tank" or i.second->name == "PlayerTank"){
+            if(i.second->serialise().type == "Tank" or i.second->serialise().type == "PlayerTank"){
                 Point point = i.second->get_point();
                 addObject(point.x, point.y, "Explosion");
             }
