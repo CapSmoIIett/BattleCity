2a2eaf604256ce67e8b15f708ff2fecd7292af8c
kociarownica <anastasiyaholupova@gmail.com>
2021-05-16 13:24:20 +0300

    Можно выбирать режим игры

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main() {
     Controller controller;
     Controller controller2;
     controller.setStartXY(16 * 3 * 8, 24 * 3 * 8);
-    controller2.setStartXY(16 * 3 * 4, 24 * 3 * 8);
+    controller2.setStartXY(8 * 3 * 8, 24 * 3 * 8);
     draw_scene.synchronize(&scene);/**/
 
     Clock clock;
@@ -40,8 +40,84 @@ int main() {
     {
     case 1:while (window.isOpen()) 
         {
+            while (window.pollEvent(event)) 
+            {
+                /**/
+                if (event.type == sf::Event::KeyPressed){
+                    switch (event.key.code)
+                    {
+                    case sf::Keyboard::Up: {
+                        controller.setUp();
+                        break;
+                    }
+                    case sf::Keyboard::Down: {
+                        controller.setDown();
+                        break;
+                    }
+                    case sf::Keyboard::Left: {
+                        controller.setLeft();
+                        break;
+                    }
+                    case sf::Keyboard::Right: {
+                        controller.setRight();
+                        break;
+                    }
+                    }
+                }
 
 
+                else if (event.type == sf::Event::KeyReleased){
+                    switch (event.key.code)
+                    {
+                    case sf::Keyboard::Up:  {
+                        controller.stop(UP);
+                        break;
+                    }
+                    case sf::Keyboard::Down: {
+                        controller.stop(DOWN);
+                        break;
+                    }
+                    case sf::Keyboard::Left: {
+                        controller.stop(LEFT);
+                        break;
+                    }
+                    case sf::Keyboard::Right: {
+                        controller.stop(RIGHT);
+                        break;
+                    }
+                    }
+                }
+                if (Keyboard::isKeyPressed(Keyboard::Space)){ controller.shoot(&scene);    }
+                
+                if (Keyboard::isKeyPressed(Keyboard::Q))    { window.close();       };
+                if (event.type == Event::Closed)
+                    window.close();
+                //else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+                //scene.map_objects[5]->set
+            }
+            
+
+            controller.manageTank(&scene);/* */
+            //controller2.manageTank(&scene);
+
+            ai_scene.synchronize(&scene);
+            ai_scene.setCommands(&scene);
+            ai_scene.manageAllAITanks(&scene);
+            scene.handleTickAll();
+            scene.clearDead();
+            draw_scene.synchronize(&scene); 
+            window.clear();     // window.clear(sf::Color(34,15,6));
+            draw_scene.draw(window, &scene);
+        
+            window.display();
+            Time elapsed_time = clock.getElapsedTime() % cycle_time;
+            //std::cout << "отрисовано за " << elapsed_time.asMilliseconds() << " миллисекунд\n";
+            sleep(cycle_time - elapsed_time);
+        }
+        break;
+
+        case 2:while (window.isOpen()) 
+        {
             while (window.pollEvent(event)) 
             {
                 /**/
@@ -134,7 +210,7 @@ int main() {
             
 
             controller.manageTank(&scene);/* */
-            //controller2.manageTank(&scene);
+            controller2.manageTank(&scene);
 
             ai_scene.synchronize(&scene);
             ai_scene.setCommands(&scene);
@@ -151,8 +227,6 @@ int main() {
             sleep(cycle_time - elapsed_time);
         }
         break;
-
-        case2: cout << "2";
 
         default:
             break;
